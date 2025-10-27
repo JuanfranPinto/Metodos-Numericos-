07fefe38f33c230ac3653bf15d748b62ce0bb94e
JuanfranPinto <pintoandrangofrancisco@gmail.com>
2025-10-27 16:39:06 -0500

    Add initial implementation of various numerical methods and examples

---
+++ eje1.py
@@ -0,0 +1,137 @@
+
+
+
+#print ("Mi\nnombre\nes\nBond.",end=" ")
+# print ("James Bond.")
+
+#print (sep="&", "fish" , "chips")
+
+#print ("2")
+#print (2)
+#print (0o0123)
+#print  (0x123)
+#print (3e8)
+#print ( 6.62607E-34)
+#print(0.0000000000000000000001)
+#print("Me gusta \"Monty Python\"")
+#print ('I\'m Monty Python.')
+#print(True > False)
+#print(True < False)
+#print (  "" estoy "" )
+#print (  """aprendiendo""")
+#print ( """" Python """"")
+''' var = (2)
+print (var)
+var +=2
+print (var)
+
+var = 4 
+print (var) 
+
+var += 1
+print (var)'''
+
+
+'''print("Dime lo que sea...")
+anything = input()
+print("Hmm...", anything, "... ¿en serio?")'''
+
+#print("quien fe el creador de la bomba atomica ")
+#anything = input()
+#print("el creador fei", anything, "si lo llamand el padre de la bomba atomica") 
+
+#print("dime lo que sea..")
+#anything = input()
+#print("hmm...", anything, "... en serio?")
+
+#3print("12 es un numero primo ")
+#anything = input()
+#print("12  es un numero primo", anything, "no  lo es")
+
+#anything = input("la pera es una fruta")
+#print ("no creo ", anything , " pero confio")
+
+#anything = input("Ingresa un número: ")
+#3something = anything ** 2.0
+#print(anything, "al cuadrado es", something)
+
+#anything = float(input("Ingresa un número: "))
+#something = anything ** 2.0
+#print(anything, "a la potencia de 2 es", something)
+
+#leg_a = float(input("Ingresa la longitud del primer cateto: "))
+#leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+#hypo = (leg_a**2 + leg_b**2) ** .5
+#print("La longitud de la hipotenusa es:", hypo)
+
+#leg_a = float(input("Ingresa la longitud del primer cateto: "))
+#leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+#print("La longitud de la hipotenusa es:", (leg_a**2 + leg_b**2) ** .5)
+
+
+#fnam = input("¿Me puedes dar tu nombre por favor? ")
+#lnam = input("¿Me puedes dar tu apellido por favor? ")
+#print("Gracias. ")
+#print("\nTu nombre es " + fnam + " " + lnam + ".")
+
+
+#print("+" + 10 * "-" + "+")
+#print(("|" + " " * 10 + "|\n") * 5, end="")
+#print("+" + 10 * "-" + "+")
+
+#print("*")
+#print( "*" * 2)
+#print ("x" *3)
+
+
+#leg_a = float(input("Ingresa la longitud del primer cateto: "))
+#leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+#print("La longitud de la hipotenusa es " + str((leg_a**2 + leg_b**2) ** .5))
+
+
+#3a = float(input("Ingresa el primer valor: "))
+#b = float(input("Ingresa el segundo valor: "))
+#print("Suma:", a + b)
+#print("Resta:", a - b)
+#print("Multiplicación:", a * b)
+#print("División:", a / b)
+#print("\n¡Eso es todo, amigos!")
+
+ 
+#a = float(input ("ingresa el valor de a "))
+#print (" el valor de la operacion es ", 1/(a+1/(a+(1/(a+(1/a))))))
+
+#hora = float (input("ingresa la hora "))
+#hora_final = hora + 00.59
+#print (" el evento termina a las " , hora_final )
+
+'''name = input("Ingresa tu nombre: ")
+print("Hola, " + name + ". '¡Gusto en conocerte'!")
+ 
+print("\nPresiona Enter para terminar el programa.")
+input()
+print("FIN.")'''
+
+#x= 1// 2 *3 
+#print ("x es igual a" , x)
+
+#x = input()
+#y = int(input())
+
+#print(x * y)
+
+#x = int(input())
+#y = int(input())
+
+#p#rint(x + y)
+
+print (" Hola Zoila como estas ")
+print ("tu hijo juenfrancisco te quiere mucho ")
+
+print
+
+
+
+
+
+
